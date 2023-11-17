27d825a85b6d92c99892d5b8aa8e9f41fef4935c
nobin-seemymachines <150339029+nobin-seemymachines@users.noreply.github.com>
2023-11-17 17:28:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function MenuOpen(){
+    document.getElementById("menu-card").style.display = "block";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("menu-close").style.display = "block"
+}
+
+function MenuClose(){
+    document.getElementById("menu-card").style.display = "none";
+    document.getElementById("menu").style.display ="block";
+    document.getElementById("menu-close").style.display = "none"
+}
+
